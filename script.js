9e2ba2f336615e063ce382c04cb6731b07f16a03
kishore <kishoresukant@gmail.com>
2020-11-28 20:11:08 +0530

    final

--- script.js
+++ script.js
@@ -4,52 +4,52 @@ data = {
         {
             name: "Oscilloscope",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP  ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "1",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "2",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "3",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "4",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "5",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "6",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "7",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "8",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         },
         {
             name: "9",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : '/img/osc.jpg'
+            image : 'img/osc.jpg'
         }
     ]
 }
