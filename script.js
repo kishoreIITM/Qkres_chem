adddb77ebc46cc1f89a25df4767b893540a603ac
kishore <kishoresukant@gmail.com>
2021-01-08 18:16:56 +0530

    Multiple images onload

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
 
+
 data = {
     products:[
         {
             name: "Oscilloscope",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP  ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/osc.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "1",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            image : 'img/osc.jpg',
+            bigimg : 'img/50.jpg'
         },
         {
             name: "2",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            image : 'img/osc.jpg',
+            bigimg : 'img/pexels-benjamin-suter-3617460.jpg'
         },
         {
             name: "3",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/pexels-karolina-grabowska-4021773.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "4",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/pexels-karolina-grabowska-4210556.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "5",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/pexels-rodolfo-clix-1366942.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "6",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/pexels-zhang-kaiyv-1168940.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "7",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/osc.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "8",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/osc.jpg',
+            image : 'img/background-2462431_1920.jpg'
         },
         {
             name: "9",
             description:"India's largest manufacturer and exporter of FRP/GRP Equipments, Field storage tanks, process equipments,high pressure vessels, Glass Flake lining,PVC/GRP ",
-            image : 'img/osc.jpg'
+            bigimg : 'img/osc.jpg',
+            image : 'img/background-2462431_1920.jpg'
         }
     ]
 }
 
-console.log(data.products[0].description);
+
 let items = data.products.map((product) => {
+    
 return(
 `<div  class="col-12 col-sm-4 wow fadeInUp slow">
 <div class="card">
@@ -70,16 +82,61 @@ return(
             ${product.description}
         </p>
         <button class="btn">
-            View more
+            Add to cart
         </button>
     </div>
 </div>
 </div>`
+)
+});
 
-
-)});
 let s = "";
 let final = items.map((item)=>{
     s = s + item;
 })
 document.getElementById('main').innerHTML= s ;
+setTimeout(function(){
+    loadedcount = 0;
+var count = data.products.length;
+for(var i=0;i <count;i++){
+    var img = new Image();
+    img.onload = function(){
+        loadedcount = loadedcount+1;
+        if (loadedcount==count){
+            console.log("loaded")
+            let items1 = data.products.map((product) => {
+            return(
+                `<div  class="col-12 col-sm-4 wow fadeInUp slow">
+                <div class="card">
+                    <div class="card-img">
+                    <img src=${product.bigimg}></img>
+                    </div>
+                    <div class="class-body text-center">
+                        <h4 class="card-title">
+                            ${product.name}
+                        </h4>
+                        <p class="card-text">
+                            ${product.description}
+                        </p>
+                        <button class="btn">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+                </div>`
+            )
+                
+        });
+        let r = "";
+        let final = items1.map((item)=>{
+            r = r + item;
+        })
+        document.getElementById('main').innerHTML= r ;
+        }
+    };
+    img.src = data.products[i].bigimg;
+}
+},3000)
+
+
+
